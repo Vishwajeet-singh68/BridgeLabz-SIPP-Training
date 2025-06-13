a3902c95d1d81a15bf26fb62b2c8b5fcb62e5241
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-06-13 11:00:51 +0530

    [Vishwajeet Singh] .add sum of n natural number

---
+++ controlFlow/SumOfNNaturalNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumOfNNaturalNumber{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int sum=n*(n+1)/2;
+        if(n>=0)
+            System.out.println("The Sum of "+n+" natural number is "+sum);
+        else    
+            System.out.println("The Numeber "+n+"is not Natural Number.");
+    }
+}
